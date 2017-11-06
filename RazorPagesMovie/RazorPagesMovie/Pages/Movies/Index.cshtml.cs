85842fc6000ec2345935f0a920691a4a647ff29b
Gregory Dukes <kcils360@gmail.com>
2017-11-06 12:05:21 -0800

    added genre search

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPagesMovie.Pages.Movies
 {
@@ -19,9 +20,16 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync(string searchString)
+        public async Task OnGetAsync(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
@@ -29,6 +37,12 @@ namespace RazorPagesMovie.Pages.Movies
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
 
             Movie = await movies.ToListAsync();
         }
