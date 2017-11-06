d3f7b7a954fad55e1cf805fcad31685233a65811
Gregory Dukes <kcils360@gmail.com>
2017-11-06 11:48:34 -0800

    updated index.cshtml.cs to handle search feature

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -20,8 +20,16 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString)
         {
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
             Movie = await _context.Movie.ToListAsync();
         }
     }
